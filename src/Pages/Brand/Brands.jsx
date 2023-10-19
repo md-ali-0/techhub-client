0b85eb71d54688b2b19fdf25d7ce5e0ca0572206
md-ali-0 <mohammad.98482@gmail.com>
2023-10-20 00:41:04 +0600

    Products By Brand Page Agged With Slider advertisement

--- src/Pages/Brand/Brands.jsx
+++ src/Pages/Brand/Brands.jsx
@@ -1,6 +1,10 @@
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { useLoaderData, useParams } from 'react-router-dom';
 import ProdutcsCard from '../Product/ProdutcsCard';
-
+import ImageOne from '/slider/slider-1.png';
+import ImageTwo from '/slider/slider-2.png';
+import ImageThree from '/slider/slider-3.png';
 const Brands = () => {
     const { name } = useParams();
     const bandProducts = useLoaderData();
@@ -14,21 +18,37 @@ const Brands = () => {
                         {name}
                     </span>
                 </h3>
-                {bandProducts.length!==0?(
-                                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                                    {bandProducts.map((product) => (
-                                        <ProdutcsCard
-                                            key={product._id}
-                                            product={product}></ProdutcsCard>
-                                    ))}
-                                </div>
-                ):(
-                    <div className='flex justify-center items-center h-80'>
-                        <p className='text-xl font-bold'>No Product Found</p>
+                <Carousel
+                    showThumbs={false}
+                    showStatus={false}
+                    infiniteLoop={true}
+                    swipeable={true}
+                    interval={200}
+                    transitionTime={100}
+                    >
+                    <div>
+                        <img src={ImageThree} />
                     </div>
-                )
-                }
-
+                    <div>
+                        <img src={ImageTwo} />
+                    </div>
+                    <div>
+                        <img src={ImageOne} />
+                    </div>
+                </Carousel>
+                {bandProducts.length !== 0 ? (
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {bandProducts.map((product) => (
+                            <ProdutcsCard
+                                key={product._id}
+                                product={product}></ProdutcsCard>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="flex justify-center items-center h-80">
+                        <p className="text-xl font-bold">No Product Found</p>
+                    </div>
+                )}
             </section>
         </div>
     );
